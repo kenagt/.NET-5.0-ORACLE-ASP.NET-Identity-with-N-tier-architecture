f888e82be3a2ba4a4b8fdbf7b7d9f9381de4fe65
Kenan <begic_kenan@hotmail.com>
2022-04-19 14:11:53 +0200

    Add files via upload

---
+++ NTierOracleIdentityExample.Web/AutoMapperConfigurations/EXAMPLE_SCHEMA_Profile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using NTierOracleIdentityExample.Dll.Entities;
+using NTierOracleIdentityExample.Web.Models;
+
+namespace NTierOracleIdentityExample.Web.AutoMapperConfigurations
+{
+    public class EXAMPLE_SCHEMA_Profile : Profile
+    {
+        public EXAMPLE_SCHEMA_Profile()
+        {
+            CreateMap<Log, LogViewModel>().ReverseMap();
+            CreateMap<ExampleTable, ExampleTableViewModel>().ReverseMap();
+            CreateMap<IdentityRole, RoleViewModel>().ReverseMap();
+            CreateMap<ApplicationUser, ApplicationUserViewModel>().ReverseMap();
+        }
+    }
+}
